4f2e50fb9af992dbaec4a9468c47d9f23ffc8774
Julie Van Beethoven <jvb@Julies-MacBook-Pro.local>
2017-12-07 17:25:27 +0100

    Updated project for electron

--- src/js/classes/Game.js
+++
@@ -1,9 +0,0 @@
-import Play from './states/Play';
-
-export default class Game extends Phaser.Game {
-  constructor() {
-    super(window.innerWidth, window.innerHeight, Phaser.AUTO);
-    this.state.add(`Play`, Play);
-    this.state.start(`Play`);
-  }
-}
