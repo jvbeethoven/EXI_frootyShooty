3ea262d5079261c04d06b880b215bf3f9a9ea0b9
Julie Van Beethoven <jvb@Julies-MacBook-Pro.local>
2017-12-08 16:55:21 +0100

    fixing branches error

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// import Game from './classes/Game';
+const path = require("path");
+const Game = require(path.resolve('js/classes/game'));
+// const Game = require('../classes/Game');
+
+const init = () => {
+  console.log(Game);
+  // new Game();
+};
+init();
