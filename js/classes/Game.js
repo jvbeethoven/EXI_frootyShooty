03da4b803e2acb0e3f615f18060e5788d8c10556
BaptistSt <baptist.standaert@gmail.com>
2017-12-10 17:10:01 +0100

    :rotating_light: removing linter warnings for phaser

--- js/classes/Game.js
+++ js/classes/Game.js
@@ -1,14 +1,9 @@
-const path = require("path");
-const Preload = require(path.resolve('js/classes/states/Preload'));
-const Menu = require(path.resolve('js/classes/states/Menu'));
-const Play = require(path.resolve('js/classes/states/Play'));
+const path = require(`path`);
+const Preload = require(path.resolve(`js/classes/states/Preload`));
+const Menu = require(path.resolve(`js/classes/states/Menu`));
+const Play = require(path.resolve(`js/classes/states/Play`));
 
 class Game extends Phaser.Game {
-
-  init() {
-    console.log('init game');
-  }
-
   constructor(oscMessage) {
 
     super(window.innerWidth, window.innerHeight, Phaser.AUTO, `content`);
@@ -18,12 +13,11 @@ class Game extends Phaser.Game {
     this.state.start(`Preload`);
 
     console.log(oscMessage);
-
   }
 }
 
 const handleResize = () => {
   console.log(window.innerWidth, window.innerHeight);
-}
+};
 
 module.exports = Game;
